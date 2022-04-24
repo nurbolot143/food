5a477bbfbd2459e134a89acd60c2bb3b81987e95
Nurbolot <nurbolot.boobekovv@gmail.com>
2022-04-24 23:13:12 +0300

    implemented menu items with classes

--- js/script.js
+++ js/script.js
@@ -98,16 +98,19 @@ window.addEventListener("DOMContentLoaded", () => {
   const modalTrigger = document.querySelectorAll("[data-modal]");
   const modalCloseBtn = document.querySelector("[data-close]");
 
+  const openModal = () => {
+    modal.classList.toggle("show");
+    document.body.classList.add("lock");
+    clearInterval(modalTimerId);
+  };
+
   const closeModal = () => {
     modal.classList.toggle("show");
     document.body.classList.remove("lock");
   };
 
   modalTrigger.forEach((btn) => {
-    btn.addEventListener("click", () => {
-      modal.classList.toggle("show");
-      document.body.classList.add("lock");
-    });
+    btn.addEventListener("click", openModal);
   });
 
   modalCloseBtn.addEventListener("click", closeModal);
@@ -123,4 +126,85 @@ window.addEventListener("DOMContentLoaded", () => {
       closeModal();
     }
   });
+
+  // const modalTimerId = setTimeout(openModal, 5000);
+
+  function showModalByScroll() {
+    if (
+      window.pageYOffset + document.documentElement.clientHeight >=
+      document.documentElement.scrollHeight - 1
+    ) {
+      openModal();
+      window.removeEventListener("scroll", showModalByScroll);
+    }
+  }
+
+  window.addEventListener("scroll", showModalByScroll);
+
+  //classes for  Menu cards
+
+  const menuItems = [
+    [
+      "img/tabs/vegy.jpg",
+      "Fitnes",
+      'Меню "Фитнес"',
+      'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Этоабсолютно новый продукт с оптимальной ценой и высоким качеством!',
+      2,
+    ],
+    [
+      "img/tabs/elite.jpg",
+      "Premium",
+      "Меню “Премиум”",
+      "В меню “Премиум” мы используем не только красивый дизайн упаковки,но и качественное исполнение блюд. Красная рыба, морепродукты,фрукты - ресторанное меню без похода в ресторан!",
+      5,
+    ],
+    [
+      "img/tabs/post.jpg",
+      "Simple",
+      'Меню "Постное"',
+      "Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков",
+      4,
+    ],
+  ];
+
+  class MenuCard {
+    constructor(
+      src,
+      alt,
+      title,
+      descr,
+      price,
+      parentSelector = ".menu .container"
+    ) {
+      this.src = src;
+      this.alt = alt;
+      this.title = title;
+      this.descr = descr;
+      this.price = price;
+      this.parent = document.querySelector(parentSelector);
+      this.transfer = 84.5;
+      this.changeToSom();
+    }
+
+    changeToSom() {
+      this.price *= this.transfer;
+    }
+
+    render() {
+      const element = document.createElement("div");
+      element.classList.add("menu__item");
+      element.innerHTML = `
+        <img src=${this.src} alt=${this.alt} />
+        <h3 class="menu__item-subtitle">${this.title}</h3>
+        <div class="menu__item-descr">${this.descr}</div>
+        <div class="menu__item-divider"></div>
+        <div class="menu__item-price">
+          <div class="menu__item-cost">Цена:</div>
+          <div class="menu__item-total"><span>${this.price}</span> сом/день</div>
+    `;
+      this.parent.append(element);
+    }
+  }
+
+  menuItems.forEach((el) => new MenuCard(...el).render());
 });
